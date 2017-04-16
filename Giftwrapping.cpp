a67d291057a1ca9fdbff5fb4e72511075e7a4800
harshkb <hkbansal1997@gmail.com>
2017-04-16 21:25:40 +0530

    Update Giftwrapping.cpp

--- Giftwrapping.cpp
+++ Giftwrapping.cpp
@@ -1,81 +1,92 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define x first
+#define y second
+
 using namespace std;
 
-struct Point
-{
-    int x, y;
-};
-
-                                                // To find orientation of ordered triplet (p, q, r).
-                                                // The function returns following values
-                                                // 0 --> p, q and r are colinear
-                                                // 1 --> Clockwise
-                                                // 2 --> Counterclockwise
-int orientation(Point p, Point q, Point r)
-{
-    int val = (q.y - p.y) * (r.x - q.x) - (q.x - p.x) * (r.y - q.y);
-
-    if (val == 0) return 0;                     // colinear
-    return (val > 0)? 1: 2;                     // clock or counterclock wise
+int order(pair<int,int> a, pair<int,int> b, pair<int,int> c){
+	int det = (b.x-a.x)*(c.y-b.y) - (c.x-b.x)*(b.y-a.y);
+	if(det == 0) return 0;           //points are collinear
+	if(det < 0)  return 1;           //a,b,c are clockwise 
+	return 2;						 //a,b,c are anti-clockwise
 }
 
-                                                // Prints convex hull of a set of n points.
-void convexHull(Point points[], int n)
-{
-                                                // There must be at least 3 points
-    if (n < 3) return;
-
-                                                // Initialize Result
-    vector<Point> hull;
-
-                                                // Find the leftmost point
-    int l = 0;
-    for (int i = 1; i < n; i++)
-        if (points[i].x < points[l].x)
-            l = i;
-
-                                            // Start from leftmost point, keep moving counterclockwise
-                                            // until reach the start point again.  This loop runs O(h)
-                                            // times where h is number of points in result or output.
-    int p = l, q;
-    do
-    {
-       hull.push_back(points[p]);            // Add current point to result
-         q = (p+1)%n;                       // Search for a point 'q' such that orientation(p, x,
-                                            // q) is counterclockwise for all points 'x'. The idea
-                                            // is to keep track of last visited most counterclock-
-                                            // wise point in q. If any point 'i' is more counterclock-
-                                            // wise than q, then update q.
-
-        for (int i = 0; i < n; i++)
-        {
-
-           if (orientation(points[p], points[i], points[q]) == 2)    // If i is more counterclockwise than current q, then
-                q = i;                                               // update q
-
-        }
-        p = q;                  // Now q is the most counterclockwise with respect to p
-                                // Set p as q for next iteration, so that q is added to
-                                // result 'hull'
-    } while (p != l);  // While we don't come to first point
-
-    for (int i = 0; i < hull.size(); i++)                      // Print Result
-        cout << "(" << hull[i].x << ", "
-              << hull[i].y << ")\n";
+void convexHull(vector<pair<int,int> > p){
+	int n = p.size();
+	vector<pair<int,int> > res;
+	
+	if(n < 3) return;
+		
+	int id = 0;
+	for(int i = 1;i < n;i++){
+		if(p[i].x < p[id].x) id = i;
+	}
+	
+	int a = id, b;
+	do{
+		res.pb(p[a]);
+		b = (a+1)%n;
+		
+		for(int i = 0;i < n;i++){
+			if(order(p[a],p[i],p[b]) == 2) b = i;
+		}
+		
+		a = b;
+	}while(a != id);
+	
+	//Result
+	cout<<"Points on the convex hull in anti-clockwise order are:\n";
+	for(int i = 0; i < res.size();i++)
+		cout<<"("<<res[i].x<<","<<res[i].y<<")\t";
 }
 
+void testInput(){
+	vector<pair<int,int> > p;
+	
+	p.pb(mp(1,1));
+	p.pb(mp(2,2));
+	p.pb(mp(0,3));
+	p.pb(mp(1,2));
+	p.pb(mp(3,1));
+	p.pb(mp(0,0));
+	p.pb(mp(3,3));
+	p.pb(mp(4,4));
+	
+	convexHull(p);
+}
 
-int main()
-{
-    int total;
-    cout<<"number of points "<<endl;
-    cin>>total;
-    cout<<"enter the points"<<endl;
-    Point points[total];
-    for(int i=0;i<total;i++)
-    {
-        cin>>points[i].x>>points[i].y;
-    }
-    convexHull(points, total);
-    return 0;
+void userInput(){
+	int n,x,y;
+	vector<pair<int,int> > p;
+	ifstream in;
+	in.open("100.txt");
+	in>>n;
+	for(int i = 0;i < n;i++){
+		in>>x>>y;
+		p.pb(mp(x,y));
+	}
+	
+	//Special Case: All points on convex hull
+	/*cout<<"Enter input size: ";
+	cin>>n;
+	for(int i = 1;i < n;i++){
+		p.pb(mp(i,i));
+	}
+	p.pb(mp(0,1));
+	*/
+	
+	convexHull(p);
+}
+
+int main(){
+	clock_t startTime = clock();
+	
+	userInput();
+	//testInput();
+	
+	double exeTime = double( clock() - startTime ) / (double)CLOCKS_PER_SEC;
+	cout<<endl<<exeTime<<" seconds."<<endl;
+	return 0;
 }
